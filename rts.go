4151a1012f58fcc95c5d2745374e7d30ca6dde31
Paolo Galeone <nessuno@nerdz.eu>
2021-09-26 09:34:32 +0200

    Update licensse and format

--- rts.go
+++ rts.go
@@ -1,13 +1,13 @@
 /*
-RTS: Request to Struct. Generates Go structs from a server response.
-Copyright (C) 2016 Paolo Galeone <nessuno@nerdz.eu>
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-Exhibit B is not attached; this software is compatible with the
-licenses expressed under Section 1.12 of the MPL v2.
-*/
+ * RTS: Request to Struct. Generates Go structs from a server response.
+ * Copyright (C) 2016-2021 Paolo Galeone <nessuno@nerdz.eu>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Exhibit B is not attached; this software is compatible with the
+ * licenses expressed under Section 1.12 of the MPL v2.
+ */
 
 // Package rts generates Go structs from JSON server responses.
 // It defines type names using the specified routes and skipping numbers
